d6430f80ec499a6459a3d18f0acda09a58f47913
Jay Wen <jaywen89@gmail.com>
2020-07-20 12:08:22 -0400

    bug fix

--- src/components/search.js
+++ src/components/search.js
@@ -11,7 +11,7 @@ function Search(props) {
   }
 
   return (
-    <form class="form-inline my-2 my-lg-0">
+    <form className="form-inline my-2 my-lg-0">
       <input
         className="form-control mr-sm-2"
         type="search"
