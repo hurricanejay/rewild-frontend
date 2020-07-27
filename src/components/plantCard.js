52549c55c2334689e287666777aafcd799e27729
flatironschool <danliu277@gmail.com>
2020-07-27 11:26:27 -0400

    Center button

--- src/components/plantCard.js
+++ src/components/plantCard.js
@@ -1,30 +1,23 @@
 import React, { useState } from 'react';
 
 function PlantCard(props) {
+    const { name, image, price, id } = props.plant;
+    const [showButton, setShowButton] = useState(false)
 
-   const [showButton, setShowButton]  = useState(false)
-
-    const {name, image, price, id} = props.plant;
-    
     const moreInfo = () => {
         props.history.push(`/plants/${id}`)
     }
-    
-    return(
+
+    return (
         <div className='plant-card' onClick={() => moreInfo()} >
-
             <h4>{name}</h4>
-            <img  className='image' src={image} 
-            onMouseEnter={()=>setShowButton(true)} 
-            onMouseLeave={()=>setShowButton(false)}
-            alt='plant'/>
+            <img className='image' src={image}
+                onMouseEnter={() => setShowButton(true)}
+                onMouseLeave={() => setShowButton(false)}
+                alt='plant' />
             <p>${price}</p>
-
-            {showButton && <button id='cart-btn'>Add To Cart</button> }
+            {showButton && <button id='cart-btn'>Add To Cart</button>}
         </div>
-
-
-
     )
 }
 
